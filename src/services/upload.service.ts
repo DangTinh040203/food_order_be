c0f2cad28463988939b1c455644a5033de1fb5e5
dangtinh <caodangtinh04022003@gmail.com>
2025-02-11 23:00:53 +0700

    update: signed url

--- src/services/upload.service.ts
+++ src/services/upload.service.ts
@@ -1,4 +1,9 @@
-import { PutObjectCommand } from '@aws-sdk/client-s3';
+import {
+  GetObjectCommand,
+  PutObjectCommand,
+  S3Client,
+} from '@aws-sdk/client-s3';
+import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
 
 import s3 from '@/configs/s3.config';
 import { InternalServerError } from '@/core/error.response';
@@ -7,16 +12,25 @@ import { CreatedResponse } from '@/core/success.response';
 export class UploadService {
   async uploadImageFromLocal(file: Express.Multer.File) {
     try {
-      const command = new PutObjectCommand({
+      const imageName = `${Date.now().toString()}_${file.originalname}`;
+
+      const uploadCommand = new PutObjectCommand({
         Bucket: process.env.AWS_S3_BUCKET_NAME!,
-        Key: `${file.originalname}_s${Date.now()}`,
+        Key: imageName,
         Body: file.buffer,
+        ContentType: file.mimetype,
       });
+      await s3.send(uploadCommand);
 
-      const result = await s3.send(command);
+      const signUrlcommand = new GetObjectCommand({
+        Bucket: process.env.AWS_S3_BUCKET_NAME!,
+        Key: imageName,
+      });
+      const url = await getSignedUrl(s3, signUrlcommand, { expiresIn: 3600 });
 
-      return new CreatedResponse('Created image', result);
+      return new CreatedResponse('Created image', { url });
     } catch (error) {
+      console.log('🚀 ~ UploadService ~ uploadImageFromLocal ~ error:', error);
       throw new InternalServerError();
     }
   }
