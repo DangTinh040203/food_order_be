76d867ad78f90ec974bcd975279c6a66862dc605
Nhật Toàn <161799070+NToan324@users.noreply.github.com>
2025-02-22 21:08:04 +0700

    ✨ Add Swagger integration (#21)
    * ✨ Add Swagger integration
    
    * ✨ Add Swagger integration
    
    * update swagger
    
    * update swagger
    
    * update swagger

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 
 import foodRoute from '@/routes/food.route';
+import swaggerRouter from '@/routes/swagger.router';
 import tableRoute from '@/routes/table.route';
 import testRoute from '@/routes/test.route';
 
@@ -9,5 +10,6 @@ const router = express.Router();
 router.use('/food', foodRoute);
 router.use('/table', tableRoute);
 router.use('/test', testRoute);
+router.use('/', swaggerRouter);
 
 export default router;
---
+++ src/routes/swagger.router.ts
@@ -0,0 +1,16 @@
+import express from 'express';
+import fs from 'fs';
+import swaggerUi from 'swagger-ui-express';
+import YAML from 'yaml';
+
+const router = express.Router();
+const file = fs.readFileSync('./src/swagger.yaml', 'utf8');
+const swaggerDocument = YAML.parse(file);
+
+router.use(
+  '/api-docs',
+  swaggerUi.serve,
+  swaggerUi.setup(swaggerDocument as object),
+);
+
+export default router;
